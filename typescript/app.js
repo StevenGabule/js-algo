293ed9d30a38f5677b4b21b97259978f158bf09a
John Paul Lim Gabule <lucas.gabule@gmail.com>
2021-01-17 23:49:07 +0800

    destructuring

--- typescript/app.js
+++ typescript/app.js
@@ -1,4 +1,23 @@
 /*/////// rest and spread operator //////*/
 var numbers = [1, 2, 3, 4];
+// spread operator example
 console.log(Math.max(33, 44, 55, 66));
 console.log(Math.max.apply(Math, numbers));
+// rest operator example - run in calling fun.
+function makeArray(name) {
+    var args = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        args[_i - 1] = arguments[_i];
+    }
+    return args;
+}
+console.log(makeArray("Max", 1, 2, 3, 4));
+/*////// destructuring ////// */
+// array
+var myHobbies = ['Reading manga', 'Watch Movies Online'];
+var hb1 = myHobbies[0], hb2 = myHobbies[1];
+console.log(hb1, hb2);
+// objects
+var userData = { username: "John", age: 27 };
+var username = userData.username, age = userData.age;
+console.log(username, age);
--- typescript/app.ts
+++ typescript/app.ts
@@ -1,4 +1,27 @@
 /*/////// rest and spread operator //////*/
 const numbers = [1, 2, 3, 4];
+
+// spread operator example
 console.log(Math.max(33, 44, 55, 66));
 console.log(Math.max(...numbers));
+
+// rest operator example - run in calling fun.
+function makeArray(name: string, ...args: number[]) {
+    return args;
+}
+
+console.log(makeArray("Max", 1, 2, 3, 4));
+
+/*////// destructuring ////// */
+// array
+const myHobbies = ['Reading manga', 'Watch Movies Online'];
+const [hb1, hb2] = myHobbies;
+console.log(hb1, hb2);
+
+// objects
+const userData = { username: "John", age: 27};
+const {username, age} = userData;
+console.log(username, age);
+
+
+
