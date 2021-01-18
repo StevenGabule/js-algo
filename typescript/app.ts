f786870611e044b55d5376d630f4d3eb639311c2
John Paul Lim Gabule <lucas.gabule@gmail.com>
2021-01-18 20:29:04 +0800

    classes

--- typescript/app.ts
+++ typescript/app.ts
@@ -24,4 +24,18 @@ const {username, age} = userData;
 console.log(username, age);
 
 
+/* CREATING A CLASSES AND CLASS PROPERTIES*/
+class Person {
+    name: string;
+    private type: string;
+    protected age: number;
+
+    constructor(name:string, public username: string) {
+        this.name = name;
+    }
+}
+
+const person = new Person('JOHN', 'john');
+console.log(person.name, person.username);
+
 
