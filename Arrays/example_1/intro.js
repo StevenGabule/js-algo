21ebf5ceb9dc0ae1bd88764c37f5d0bc2fa2919f
John Paul Lim Gabule <lucas.gabule@gmail.com>
2021-10-08 14:00:32 +0800

    aray

--- Arrays/example_1/intro.js
+++ Arrays/example_1/intro.js
@@ -1,42 +1,50 @@
-// Webstorm VS vscode
+// Webstorm -paid ide VS VSCode
 
 // Create an Array
-// let fruits = ['Apple', 'Banana']
 // console.log(fruits.length) // 2
-
-// Access an Array item using the index position
+//
+// // Access an Array item using the index position
 // let first = fruits[0];
-// let last = fruits[fruits.length-1];
+// let last = fruits[fruits.length - 1];
 // console.log(first, last)
 
+
+// loop every item in the array
 // fruits.forEach(function(item, index, array) {
-  // parseInt = convert data to integer
-  // parseFloat = convert data to float
-  // if (index === Number("1")) {
-  //   console.log('Apple is my favorite')
-  // }
-  // console.log(item, index)
+//   console.log(item, index)
 // })
-// add an item to the end of an array
+//
+// let fruits = ['Apple', 'Banana']
+// // add an item to the end of an array
 // fruits.push('orange')
 // console.log(fruits)
-
-// remove an item to the end of an array
+//
+// // remove an item to the end of an array
 // fruits.pop()
 // console.log(fruits)
-
-// delete the front
+//
+// // delete the front
 // fruits.shift()
 // console.log(fruits)
-
-// add to the front array
+//
+// // add to the front array
 // fruits.unshift('strawberry', 'Can', 'Dog')
 // console.log(fruits)
-
+//
 // let pos = fruits.indexOf('strawberry')
 // console.log(pos) // 0
+//
+// let letters = ["a", "b", "c", "d", "e", "f", "g"]
+// let removeItem = letters.splice(0, 2);
+// console.log("removeItem:" + removeItem)
+// console.log(letters)
 
-let letters = ["a", "b", "c", "d", "e", "f", "g"]
-let removeItem = letters.splice(1, 1);
-console.log("removeItem:" + removeItem)
-console.log(letters)
+let newArr1 = [1, 2, 3, 4, 5]
+let newArr2 = [6, 7, 8, 9, 10]
+let newArr3 = [16, 17, 18, 19, 101]
+const totalArr = newArr1.concat(newArr2, newArr3)
+// console.log(totalArr)
+
+// check if the value is less than to 10
+const lessThanValue = totalArr.filter((val) => val < 10)
+console.log(lessThanValue)
--- Arrays/example_1/intro_code.js
+++ Arrays/example_1/intro_code.js
@@ -1,54 +1,41 @@
-const multiArr = [
+/*const multiArr = [
   [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
   [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 ]
+console.log([1,2,3,4,5].map(num => num * 10))*/
 
-console.log([1,2,3,4,5].map(num => num * 10))
+const persons = [
+  {name: "John paul", age: 28, school: "ACLC", salary: 20000, history: [24000, 25500, 10000]},
+  {name: "John doe", age: 10, school: "CMU", salary: 28000, history: [25000, 25500, 10000]},
+  {name: "Jane doe", age: 31, school: "BUKSO", salary: 32000, history: [20000, 25500, 10000]},
+];
 
-// const multiArrObj = [
-//   {
-//     name: "John paul",
-//     age: 28,
-//     school: "ACLC"
-//   },
-//   {
-//     name: "John doe",
-//     age: 10,
-//     school: "CMU"
-//   },
-//   {
-//     name: "Jane doe",
-//     age: 31,
-//     school: "BUKSO"
-//   },
-// ]
-// // deconstruct
-// // const {name, age, school} = person
-// multiArrObj.map(({name, age, school}) => {
-//   // if (age > 50) {
-//   //   console.log('your so old!')
-//   // }
-//   // console.log(name, age + 10, school)
+// calculate the history salary
+// persons.map(person => {
+//   const personSalaryHistory = person.history.reduce((prev, curr) => prev + curr)
+// console.log(`${person.name} total salary in this year: ${personSalaryHistory}`)
+// console.log(person.name, person.age, person.school, person.salary)
 // })
+
 //
-// const newFilterArr = multiArrObj.filter((person) => person.age === 10) // compare
-// console.log(newFilterArr)
+// const personThatAge28 = persons.filter((person) => person.age === 28) // compare
+// console.log(personThatAge28[0].name)
 //
 //
-// /*
-// * The Array.from() static method creates a new, shallow-copied
-// * Array instance from an array-like or iterable object.
-// * */
+/*
+* The Array.from() static method creates a new, shallow-copied
+* Array instance from an array-like or iterable object.
+* */
 // console.log(Array.from('foo'));
 // console.log(Array.from([1, 2, 3], x => x + x));
 //
 // /*
 // * The Array.isArray() method determines whether the passed value is an Array.
 // * */
-// Array.isArray([1, 2, 3]);  // true
-// Array.isArray({foo: 123}); // false
-// Array.isArray('foobar');   // false
-// Array.isArray(undefined);  // false
+Array.isArray([1, 2, 3]);  // true
+Array.isArray({foo: 123}); // false
+Array.isArray('foobar');   // false
+Array.isArray(undefined);  // false
 // // console.log(Array.isArray({}))
 //
 // /*
@@ -61,3 +48,28 @@ console.log([1,2,3,4,5].map(num => num * 10))
 //
 // Array.of(1, 2, 3); // [1, 2, 3]
 // Array(1, 2, 3);    // [1, 2, 3]
+
+
+/* //// PROBLEM ////
+* Create an array with values of numbers and map every items and
+* multiply by 5 and then display only that greater than or equal to 20.
+* */
+// console.log([1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(num => num * 5).filter(num => num >= 20))
+
+// const numbers = [2, 3, 4]
+// const result = numbers.filter((number) => (number >= 20));
+// console.log(result);
+// const numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const lessThanValue = numArr.filter((val) => val >= 20)
+// console.log(lessThanValue);
+
+
+// const numbers = [1,2,3,4,5];
+// const newArr = numbers.map(myFunction);
+//
+// function myFunction(num) {
+//   return num * 5;
+// }
+//
+// const greaterthanValue =newArr.filter((val) => val >= 20)
+// console.log(greaterthanValue);
